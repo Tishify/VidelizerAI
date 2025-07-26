430c6c39717d084ddacbcc52a948f61d2f0042b5
Developer <developer@company.com>
2025-07-26 14:47:03 +0300

    DockerTest

--- Transcription/app/main.py
+++ Transcription/app/main.py
@@ -5,3 +5,34 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"status": "Videlizer AI Whisper API is running"}
+
+
+
+# from fastapi import FastAPI, UploadFile, File
+# from fastapi.responses import JSONResponse
+# import whisper
+# import shutil
+# import uuid
+# import os
+
+# app = FastAPI()
+
+# # Загружаем модель один раз при старте
+# model = whisper.load_model("medium")
+
+# UPLOAD_DIR = "temp_uploads"
+# os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+# @app.post("/transcribe")
+# async def transcribe(file: UploadFile = File(...)):
+#     try:
+#         # Генерируем уникальное имя
+#         file_id = str(uuid.uuid4())
+#         file_path = os.path.join(UPLOAD_DIR, f"{file_id}_{file.filename}")
+
+#         # Сохраняем файл
+#         with open(file_path, "wb") as buffer:
+#             shutil.copyfileobj(file.file, buffer)
+
+#         # Транскрибируем
+#         result = mod
